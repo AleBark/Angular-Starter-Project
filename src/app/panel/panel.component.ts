7bde20f629ee52be5eeed9805ad502280d11aeac
Ale Bark <ale.bark_bruneri@hotmail.com>
2018-11-08 23:56:53 -0200

    Two way binding on answer update and progress bar updates

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -11,21 +11,38 @@ export class PanelComponent implements OnInit {
 
   public questions: Array<Questions> = QUESTIONS;
   public infos: Array<String> = ['Progress', 'Lives', 'Question:'];
-  public answer: String;
+  public answer: String = '';
   public round = 0;
+  public barPercentage = 0;
   public roundQuestion: Questions;
 
   constructor() {
-    this.roundQuestion = this.questions[this.round];
+    this.roundUpdate();
   }
 
   ngOnInit() {
   }
 
+  public roundUpdate(): void {
+    this.roundQuestion = this.questions[this.round];
+  }
+
   public checkAnswer (): void {
 
+    if (this.roundQuestion.answer === this.answer) {
+
+    alert('Right!');
     this.round++;
-    this.roundQuestion = this.questions[this.round];
+    this.barPercentage = this.barPercentage + (100 / this.questions.length );
+    this.roundUpdate();
+    this.answer = '';
+
+    } else {
+
+      alert('Wrong!');
+
+    }
+
   }
 
   public updateAnswer (answer: Event): void {
--- src/app/panel/questions-mock.ts
+++ src/app/panel/questions-mock.ts
@@ -3,6 +3,7 @@ import { Questions } from '../shared/questions.model';
 export const QUESTIONS: Array<Questions> = [
     { question: 'xxxx', answer: 'x2' },
     { question: 'yyyy', answer: 'y2' },
+    { question: 'wwww', answer: 'w2' },
     { question: 'zzzz', answer: 'z2' },
 ];
 
