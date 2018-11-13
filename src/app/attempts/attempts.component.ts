04d83e47122f82cbc17d03da2ffb9e2f886604bc
Ale Bark <ale.bark_bruneri@hotmail.com>
2018-11-12 23:05:39 -0200

    Decreasing lives

--- src/app/attempts/attempts.component.ts
+++ src/app/attempts/attempts.component.ts
@@ -1,12 +1,13 @@
 import { Heart } from '../shared/hearts.model';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 
 @Component({
   selector: 'app-attempts',
   templateUrl: './attempts.component.html',
   styleUrls: ['./attempts.component.css']
 })
-export class AttemptsComponent implements OnInit {
+export class AttemptsComponent implements OnChanges {
+
 
   public lives: Array<Heart> = [new Heart(true), new Heart(true), new Heart(true)];
   @Input() public attempts: number;
@@ -15,8 +16,19 @@ export class AttemptsComponent implements OnInit {
 
   }
 
-  ngOnInit() {
+
+  ngOnChanges(): void {
+    // Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    // Add '${implements OnChanges}' to the class.
     console.log(this.attempts);
+    if (this.attempts !== this.lives.length) {
+        this.lives[(this.lives.length - this.attempts) - 1].isFull = false;
+    }
   }
+
+  //  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
+  // }
 
 }
